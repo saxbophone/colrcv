397e2742bcc1a6b3ccc97dfbb658ac17ddc3a422
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2017-02-17 00:51:11 +0000

    Refactored out all of the shared parts of the HSL/HSV algorithm These are now in separate private functions and can be used by the RGB->HSL code which will be written next

--- colrcv/models/rgb.c
+++ colrcv/models/rgb.c
@@ -51,17 +51,58 @@ bool colrcv_rgb_is_valid(colrcv_rgb_t rgb) {
     );
 }
 
+/* BEGIN HSL/HSV private support functions */
+
+// Scales down RGB amounts from 0->255 to 0->1
+static void scale_down_rgb(colrcv_rgb_t rgb, double* r, double*g, double* b) {
+    *r = rgb.r / 255;
+    *g = rgb.g / 255;
+    *b = rgb.b / 255;
+}
+
+// sets the minimum, maximum and delta for a given set of rgb amounts
+static void get_min_max_delta(
+    double r, double g, double b, double* min, double* max, double* delta
+) {
+    *min = colrcv_min(r, colrcv_min(g, b));
+    *max = colrcv_max(r, colrcv_max(g, b));
+    *delta = *max - *min;
+}
+
+// returns the hue amount of HSV/HSL for given r, g, b, max and delta
+static double get_hue_amount(
+    double r, double g, double b, double max, double delta
+) {
+    // set the hue based on delta and which channel had the highest value
+    // (http://wiki.secondlife.com/wiki/Color_conversion_scripts)
+    double hue;
+    if(r == max) {
+        hue = (g - b) / delta;
+    } else if(g == max) {
+        hue = 2 + (b - r) / delta;
+    } else { // b == max
+        hue = 4 + (r - g) / delta;
+    }
+    // adjust hue to be in the 0-360 range
+    hue *= 60;
+    // wrap-around the output value of hue if needed
+    if(hue < 0) {
+        hue += 360;
+    } else if(hue > 360) {
+        hue -= 360;
+    }
+    return hue;
+}
+
+/* END HSL/HSV private support functions */
+
 // Algorithm: http://www.easyrgb.com/index.php?X=MATH&H=20#text20
 colrcv_result_t colrcv_rgb_to_hsv(colrcv_rgb_t rgb, colrcv_hsv_t* hsv) {
-    // scale down each RGB channel to be in the range 0-1
-    double r = rgb.r / 255;
-    double g = rgb.g / 255;
-    double b = rgb.b / 255;
-    // get the minimum and maximum channel amount
-    double min_channel = colrcv_min(r, colrcv_min(g, b));
-    double max_channel = colrcv_max(r, colrcv_max(g, b));
-    // get the difference (delta) between max and min
-    double delta_channel = max_channel - min_channel;
+    double r, g, b, min_channel, max_channel, delta_channel;
+    // scale down each RGB channel
+    scale_down_rgb(rgb, &r, &g, &b);
+    // get min and max of these channels and the delta of min and max
+    get_min_max_delta(r, g, b, &min_channel, &max_channel, &delta_channel);
     // the value component is set to max_channel (upscaled to 0-100 range)
     hsv->v = max_channel * 100;
     // if delta is 0, this is an achromatic grey
@@ -71,23 +112,8 @@ colrcv_result_t colrcv_rgb_to_hsv(colrcv_rgb_t rgb, colrcv_hsv_t* hsv) {
     } else {
         // set saturation and upscale to the 0-100 range
         hsv->s = (delta_channel / max_channel) * 100;
-        // set the hue based on delta and which channel had the highest value
-        // (http://wiki.secondlife.com/wiki/Color_conversion_scripts)
-        if(r == max_channel) {
-            hsv->h = (g - b) / delta_channel;
-        } else if(g == max_channel) {
-            hsv->h = 2 + (b - r) / delta_channel;
-        } else { // b == max_channel
-            hsv->h = 4 + (r - g) / delta_channel;
-        }
-        // adjust h to be in the 0-360 range
-        hsv->h *= 60;
-        // wrap-around the output value of h if needed
-        if(hsv->h < 0) {
-            hsv->h += 360;
-        } else if(hsv->h > 360) {
-            hsv->h -= 360;
-        }
+        // finally, set the hue
+        hsv->h = get_hue_amount(r, g, b, max_channel, delta_channel);
     }
     return;
 }
